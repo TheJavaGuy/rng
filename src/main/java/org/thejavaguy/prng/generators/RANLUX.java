f246a25eecc968995b88034b61e9e8fecc978317
Ivan Milosavljevic (TheJavaGuy) <TheJavaGuy@yandex.com>
2016-11-26 11:40:10 +0100

    Deleted unneeded resources, RANLUX generator (I will not implement it at all) and RNGService (functionality will be in the PRNG.Smart class)

--- src/main/java/org/thejavaguy/prng/generators/RANLUX.java
+++
@@ -1,9 +0,0 @@
-package org.thejavaguy.prng.generators;
-
-public final class RANLUX implements PRNG {
-    @Override
-    public int nextInt() {
-        // TODO Auto-generated method stub
-        return 0;
-    }
-}
