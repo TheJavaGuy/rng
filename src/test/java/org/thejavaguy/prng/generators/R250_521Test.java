dc8fb4793a91a27c7c53b529bb9fe22a200c5851
Ivan Milosavljevic (TheJavaGuy) <TheJavaGuy@yandex.com>
2016-11-26 12:10:31 +0100

    Refactored tests so that all common tests are moved to its own class

--- src/test/java/org/thejavaguy/prng/generators/R250_521Test.java
+++ src/test/java/org/thejavaguy/prng/generators/R250_521Test.java
@@ -2,9 +2,9 @@ package org.thejavaguy.prng.generators;
 
 import org.thejavaguy.prng.generators.PRNG;
 
-public final class R250Test extends CommonTests {
+public final class R250_521Test extends CommonTests {
     @Override
     public PRNG.Smart generator() {
-        return new R250.Smart(new R250());
+        return new R250_521.Smart(new R250_521());
     }
 }
